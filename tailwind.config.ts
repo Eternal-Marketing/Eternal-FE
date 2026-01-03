3a77fc7fd3b6f0a86856408e6451c02a134b4aa8
seungmin <daniel939015@gmail.com>
2026-01-04 04:31:44 +0900

    디자인 시스템 기본 구조 세팅 (컬러, 타이포그래피, 폰트 초안)

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#184BBA",
+        bg: "#FFFFFF",
+
+        main: "#111111",
+        inverse: "#FFFFFF",
+
+        sub1: "#505050",
+        sub2: "#767676",
+        sub3: "#999999",
+      },
+    },
+  },
+};
+
+export default config;
