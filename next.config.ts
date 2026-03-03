4a8cb2cbfa023b825039b317f1ddb16bd9635043
노승민 <daniel939015@gmail.com>
2026-03-04 03:15:15 +0900

    feat:ourteam부분svg코드로변환

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
+    qualities: [75, 90, 95, 100],
     formats: ['image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
